f241b672e785d5a2bdb295da500e4223f539d12a
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-11-08 19:25:24 +0100

    LoginVC ui

---
+++ Smack/Controller/LoginVCViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVCViewController.swift
+//  Smack
+//
+//  Created by Nikita on 08/11/2017.
+//  Copyright © 2017 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class LoginVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Nikita on 08/11/2017.
+//  Copyright © 2017 Nikita. All rights reserved.
+//
+
+import Foundation
+// Segues
+let TO_LOGIN = "toLogin"
